08f2ae1f4a0db1a3621234b1dee76d2967a605de
Arif Dinulsyah Putra <arif.dinulsyah.p@mail.ugm.ac.id>
2019-03-15 07:20:57 +0700

    update exercise-3

--- week2/exercise-3.js
+++ week2/exercise-3.js
@@ -1,26 +1,26 @@
 var nama = 'arif'
-var peran = 'ksatria'
+var peran = 'Tabib'
 
-if (nama === '' && peran === '' || nama === ''){
+if (!nama){
   console.log('Nama harus diisi!')
 }
-else if (nama !== '' && peran === ''){
+else if (!peran){
   console.log('Hallo '+ nama + ', Pilih peranmu untuk memulai game!')
 }
-else if (nama !== '' && peran === 'Ksatria'){
+else if (nama && peran === 'Ksatria'){
   console.log('Selamat datang di Dunia Proxytia, '+nama)
   console.log('Halo Ksatria '+nama+ ', kamu dapat menyerang dengan senjatamu!')
 }
 
-else if (nama !== '' && peran === 'Tabib'){
+else if (nama && peran === 'Tabib'){
   console.log('Selamat datang di Dunia Proxytia, '+nama)
   console.log('Halo Tabib '+nama +', kamu akan membantu temanmu yang terluka.')
 }
 
-else if (nama !== '' && peran === 'Penyihir'){
+else if (nama && peran === 'Penyihir'){
   console.log('Selamat datang di Dunia Proxytia, '+nama)
   console.log('Halo Penyihir '+nama +', ciptakan keajaiban yang membantu kemenanganmu!')
 }
 else{
-  console.log('Tidak ada peran tersebut, Peran yang tersedia adalah: "Tabib", "Ksatria","Penyihir"')
+  console.log(false)
 }
