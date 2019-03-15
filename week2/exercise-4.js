5741855bf1c3150e8e21145a6d5bcce39f0d985e
Arif Dinulsyah Putra <arif.dinulsyah.p@mail.ugm.ac.id>
2019-03-15 07:29:49 +0700

    update exercise-4.js

--- week2/exercise-4.js
+++ week2/exercise-4.js
@@ -1,45 +1,49 @@
 var tanggal = 17
 var bulan = 8
 var tahun = 1945
-
-switch(bulan){
-  case 1:
-    console.log(tanggal+' Januari '+tahun)
-    break;
-  case 2:
-    console.log(tanggal+' Februari '+tahun)
-    break;
-  case 3:
-    console.log(tanggal+' Maret '+tahun)
-    break;
-  case 4:
-    console.log(tanggal+' April '+tahun)
-    break;
-  case 5:
-    console.log(tanggal+' Mei '+tahun)
-    break;
-  case 6:
-    console.log(tanggal+' Juni '+tahun)
-    break;
-  case 7:
-    console.log(tanggal+' Juli '+tahun)
-    break;
-  case 8:
-    console.log(tanggal+' Agustus '+tahun)
-    break;
-  case 9:
-    console.log(tanggal+' September '+tahun)
-    break;
-  case 10:
-    console.log(tanggal+' Oktober '+tahun)
-    break;
-  case 11:
-    console.log(tanggal+' November '+tahun)
-    break;
-  case 12:
-    console.log(tanggal+' Desember '+tahun)
-    break;
-  default:
-    console.log('salah bulan')
-    break;
+if(tanggal >= 1 && tanggal <= 31 && tahun >= 1900 && tahun <= 2200){
+   switch(bulan){
+    case 1:
+      console.log(tanggal+' Januari '+tahun)
+      break;
+    case 2:
+      console.log(tanggal+' Februari '+tahun)
+      break;
+    case 3:
+      console.log(tanggal+' Maret '+tahun)
+      break;
+    case 4:
+      console.log(tanggal+' April '+tahun)
+      break;
+    case 5:
+      console.log(tanggal+' Mei '+tahun)
+      break;
+    case 6:
+      console.log(tanggal+' Juni '+tahun)
+      break;
+    case 7:
+      console.log(tanggal+' Juli '+tahun)
+      break;
+    case 8:
+      console.log(tanggal+' Agustus '+tahun)
+      break;
+    case 9:
+      console.log(tanggal+' September '+tahun)
+      break;
+    case 10:
+      console.log(tanggal+' Oktober '+tahun)
+      break;
+    case 11:
+      console.log(tanggal+' November '+tahun)
+      break;
+    case 12:
+      console.log(tanggal+' Desember '+tahun)
+      break;
+    default:
+      console.log(false)
+      break;
+  }
+}
+else{
+  console.log(false)
 }
